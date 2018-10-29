8c73640aaba8db34c8751bda54f3144f89bf3ec7
Jonas Eana <jonas@eana.ro>
2018-10-29 08:57:32 +0000

    Disable incoming SSH connections for the basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -8,8 +8,6 @@ module "eks" {
   name        = "basic"
   default_vpc = true
 
-  ssh_cidr         = "192.168.1.1/32"
-
   enable_kubectl   = true
   enable_dashboard = true
   enable_kube2iam  = true
